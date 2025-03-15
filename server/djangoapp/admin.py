389bdc68393123114f17020d1cddace907a80f3e
Z-iel <lsisuan@dso.org.sg>
2025-03-15 18:58:23 +0800

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,3 +1,19 @@
+
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+
+# Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+
+# CarModelInline class
+
+# CarModelAdmin class
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
 # from django.contrib import admin
 # from .models import related models
 
